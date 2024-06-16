fa4f83d8f665e407dc05ca30df51bcb8cfcabd0b
Shrey0207 <shreytolasaria4297@gmail.com>
2024-06-16 20:40:24 +0530

    switched to old  clerk  version

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,14 +1,9 @@
-import { auth, currentUser } from "@clerk/nextjs/server";
+import { UserButton } from "@clerk/nextjs";
 
-export default async function Page() {
-  // Get the userId from auth() -- if null, the user is not signed in
-  const { userId } = auth();
-
-  if (userId) {
-    // Query DB for user specific information or display assets only to signed in users
-  }
-
-  // Get the Backend API User object when you need access to the user's information
-  const user = await currentUser();
-  // Use `user` to render user details or create UI elements
+export default function Home() {
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
